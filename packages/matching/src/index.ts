72371f06499019a32220ff4c126f84a7126c65ea
Theo Boutron <tboutron@lssm.co>
2026-07-21 10:46:19 +0200

    fix: export candidate projector from matching package

--- packages/matching/src/index.ts
+++ packages/matching/src/index.ts
@@ -1,1 +1,6 @@
+export * from "./candidate-normalization";
+export * from "./candidate-projector";
+export * from "./candidate-types";
 export * from "./job-projection";
+export * from "./candidate-projector";
+export * from "./candidate-types";
